5c4192b033f2db361e2322dd4e27b52906a386f9
NorbertSzabo <szabo.norbert.c@gmail.com>
2016-05-30 15:20:17 +0300

    Add files via upload

---
+++ src/hello.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-hello-component',
+  template: '<h1>Hello, this is my First Angular 2 App</h1>'
+})
+export class HelloComponent { 
+
+}
